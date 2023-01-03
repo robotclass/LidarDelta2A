b6e1736d71f7c21176e481d06e0d2ef6ea42ff4d
Oleg Evsegneev <oleg.evsegneev@gmail.com>
2023-01-03 18:45:26 +0500

    Update lidar_file.py
    [+] лишнее из примера

--- examples/lidar_file.py
+++ examples/lidar_file.py
@@ -5,19 +5,10 @@
 # автор: Олег Евсегнеев
 # e-mail: oleg.evsegneev@gmail.com
 import LidarDelta2A
-import signal, sys
-import time
 import numpy as np
 
 port = 'COM10'
 lidar = LidarDelta2A.LidarDelta2A(port, baudrate = 230400) 
-
-def signal_handler(sig, frame):
-    print('Pressed Ctrl+C!')
-    lidar.stop()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
 
 while not ready:
     data = lidar.handleData()
